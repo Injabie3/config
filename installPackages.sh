ee4124c25b15b96cc10cd14d151114da5d87d6b0
Injabie3 <injabie3@gmail.com>
2024-02-10 13:02:46 -0800

    misc: Add installPackages.sh
    I find myself often forgetting which packages to install. This commit
    adds an installPackages script that will hold various packages I'll
    probably need if I ever need to re-install.

---
+++ installPackages.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env/ bash
+# Injabie3 - 2024-02-10
+# This script is for installing various packages that I'll probably be using on my
+# system.
+
+set -e
+sudo apt-get install -y htop
+sudo apt-get install -y scdaemon
+# Kerberos for AD shares
+sudo apt-get install -y krb5-user
+
+# Fusuma - Multitouch Gestures on Linux
+sudo apt-get install -y \
+    libinput-tools \
+    ruby
+sudo gem install fusuma
+
