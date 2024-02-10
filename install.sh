449d98c30a05df7fb1be67088418f3fbc1ca6afb
Injabie3 <injabie3@gmail.com>
2024-02-10 12:37:55 -0800

    misc: Remove install.sh and use stow
    This commit removes the old install.sh script in favour of using GNU
    stow, which achieves the same thing but way better.

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR=`dirname $(readlink -f $0)`
-echo $SCRIPT_DIR
-ln -s "$SCRIPT_DIR/.bashrc" ~/
-ln -s "$SCRIPT_DIR/.tmux.conf" ~/
-ln -s "$SCRIPT_DIR/.vimrc" ~/
-ln -s "$SCRIPT_DIR/.gitconfig" ~/
